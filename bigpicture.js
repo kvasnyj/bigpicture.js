846e41228dffc6da3b1261721720b551fdbfe3e9
Roman Kvasnyy <roman@mediabuyingsystems.com>
2019-01-16 08:49:23 +0100

    Use constant

--- bigpicture.js
+++ bigpicture.js
@@ -10,7 +10,7 @@
  *
  */
 
-var bigpicture = (function() {
+var bigpicture = (function () {
   "use strict";
 
   /*
@@ -32,23 +32,25 @@ var bigpicture = (function() {
   current.zoom = params.zoom ? parseFloat(params.zoom) : $(bp).data('zoom');
 
   bp.x = 0; bp.y = 0;
-  bp.updateContainerPosition = function() { bp.style.left = bp.x + 'px'; bp.style.top = bp.y + 'px'; };
+  bp.updateContainerPosition = function () { bp.style.left = bp.x + 'px'; bp.style.top = bp.y + 'px'; };
 
   /*
    * TEXT BOXES
    */
 
-  $(".text").each(function() { updateTextPosition(this); });     // initialization
+  $(".text").each(function () { updateTextPosition(this); });     // initialization
 
-  $(bp).on('blur', '.text', function() { if ($(this).text().replace(/^\s+|\s+$/g, '') === '') { $(this).remove(); } });
+  $(bp).on('blur', '.text', function () { if ($(this).text().replace(/^\s+|\s+$/g, '') === '') { $(this).remove(); } });
 
-  $(bp).on('input', '.text', function() { redoSearch = true; });
+  $(bp).on('input', '.text', function () { redoSearch = true; });
 
   function updateTextPosition(e) {
-    var size = $(e).data("size") / current.zoom;
-    var opacity = (10-size)/(200-10) + (1-0);
+    const sizeOpacity1 = 10;
+    const sizeOpacity0 = 300;
+    var fontSize = $(e).data("size") / current.zoom;
+    var opacity = (sizeOpacity1 - fontSize) / (sizeOpacity0 - sizeOpacity1) + 1;
     e.style.opacity = opacity > 0 ? opacity : 0;
-    e.style.fontSize = size + 'px';
+    e.style.fontSize = fontSize + 'px';
 
     e.style.left = ($(e).data("x") - current.x) / current.zoom - bp.x + 'px';
     e.style.top = ($(e).data("y") - current.y) / current.zoom - bp.y + 'px';
@@ -65,7 +67,7 @@ var bigpicture = (function() {
     return tb;
   }
 
-  bpContainer.onclick = function(e) {
+  bpContainer.onclick = function (e) {
     if (isContainedByClass(e.target, 'text')) { return; }
     newText(current.x + (e.clientX) * current.zoom, current.y + (e.clientY) * current.zoom, 20 * current.zoom, '').focus();
   };
@@ -78,7 +80,7 @@ var bigpicture = (function() {
     dragging = false,
     previousMousePosition;
 
-  bpContainer.onmousedown = function(e) {
+  bpContainer.onmousedown = function (e) {
     if ($(e.target).hasClass('text') && (e.ctrlKey || e.metaKey)) {
       movingText = e.target;
       movingText.className = "text noselect notransition";
@@ -91,17 +93,17 @@ var bigpicture = (function() {
     previousMousePosition = { x: e.pageX, y: e.pageY };
   };
 
-  window.onmouseup = function() {
+  window.onmouseup = function () {
     dragging = false;
     if (movingText) { movingText.className = "text"; }
     movingText = null;
   };
 
-  bpContainer.ondragstart = function(e) {
+  bpContainer.ondragstart = function (e) {
     e.preventDefault();
   };
 
-  bpContainer.onmousemove = function(e) {
+  bpContainer.onmousemove = function (e) {
     if (dragging && !e.shiftKey) {       // SHIFT prevents panning / allows selection
       bp.style.transitionDuration = "0s";
       bp.x += e.pageX - previousMousePosition.x;
@@ -123,7 +125,7 @@ var bigpicture = (function() {
    * ZOOM
    */
 
-  bpContainer.ondblclick = function(e) {
+  bpContainer.ondblclick = function (e) {
     e.preventDefault();
     onZoom((e.ctrlKey || e.metaKey) ? current.zoom * 1.7 * 1.7 : current.zoom / 1.7 / 1.7, current.x + e.clientX * current.zoom, current.y + e.clientY * current.zoom, e.clientX, e.clientY);
   };
@@ -134,7 +136,7 @@ var bigpicture = (function() {
   function onZoom(zoom, wx, wy, sx, sy) {  // zoom on (wx, wy) (world coordinates) which will be placed on (sx, sy) (screen coordinates)
     wx = (typeof wx === "undefined") ? current.x + window.innerWidth / 2 * current.zoom : wx;
     wy = (typeof wy === "undefined") ? current.y + window.innerHeight / 2 * current.zoom : wy;
-    sx = (typeof sx === "undefined") ? window.innerWidth / 2  : sx;
+    sx = (typeof sx === "undefined") ? window.innerWidth / 2 : sx;
     sy = (typeof sy === "undefined") ? window.innerHeight / 2 : sy;
 
     bp.style.transitionDuration = "0.2s";
@@ -143,7 +145,7 @@ var bigpicture = (function() {
     bp.updateContainerPosition();
     current.x = wx - sx * zoom; current.y = wy - sy * zoom; current.zoom = zoom;
 
-    $(".text").each(function() { updateTextPosition(this); });
+    $(".text").each(function () { updateTextPosition(this); });
 
     biggestPictureSeen = false;
   }
@@ -158,12 +160,14 @@ var bigpicture = (function() {
     function universeboundingrect() {
       var minX = Infinity, maxX = - Infinity, minY = Infinity, maxY = - Infinity;
       var texteelements = document.getElementsByClassName('text');
-      [].forEach.call(texteelements, function(elt) {
+      [].forEach.call(texteelements, function (elt) {
         var rect2 = elt.getBoundingClientRect();
-        var rect = { left: $(elt).data("x"),
-                     top: $(elt).data("y"),
-                     right: (rect2.width > 2 && rect2.width < 10000) ? current.x + rect2.right * current.zoom : $(elt).data("x") + 300 * $(elt).data("size") / 20,
-                     bottom: (rect2.height > 2 && rect2.height < 10000) ? current.y + rect2.bottom * current.zoom : $(elt).data("y") + 100 * $(elt).data("size") / 20 };
+        var rect = {
+          left: $(elt).data("x"),
+          top: $(elt).data("y"),
+          right: (rect2.width > 2 && rect2.width < 10000) ? current.x + rect2.right * current.zoom : $(elt).data("x") + 300 * $(elt).data("size") / 20,
+          bottom: (rect2.height > 2 && rect2.height < 10000) ? current.y + rect2.bottom * current.zoom : $(elt).data("y") + 100 * $(elt).data("size") / 20
+        };
         if (rect.left < minX) { minX = rect.left; }
         if (rect.right > maxX) { maxX = rect.right; }
         if (rect.top < minY) { minY = rect.top; }
@@ -173,7 +177,7 @@ var bigpicture = (function() {
     }
 
     var texts = document.getElementsByClassName('text');
-    if (texts.length === 0) { return; } 
+    if (texts.length === 0) { return; }
     if (texts.length === 1) { zoomOnText(texts[0]); return; }
 
     if (!biggestPictureSeen) {
@@ -199,7 +203,7 @@ var bigpicture = (function() {
 
   function find(txt) {
     results = { index: -1, elements: [], text: txt };
-    $(".text").each(function() {
+    $(".text").each(function () {
       if ($(this).text().toLowerCase().indexOf(txt.toLowerCase()) != -1) { results.elements.push(this); }
     });
     if (results.elements.length > 0) { results.index = 0; }
@@ -222,26 +226,27 @@ var bigpicture = (function() {
    * MOUSEWHEEL
    */
 
-  var mousewheeldelta = 0, 
-    last_e, 
-    mousewheeltimer = null, 
+  var mousewheeldelta = 0,
+    last_e,
+    mousewheeltimer = null,
     mousewheel;
 
   if (navigator.appVersion.indexOf("Mac") != -1) {   // Mac OS X
-    mousewheel = function(e) {
+    mousewheel = function (e) {
       e.preventDefault();
       mousewheeldelta += Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
       last_e = e;
       if (!mousewheeltimer) {
-        mousewheeltimer = setTimeout(function() {
+        mousewheeltimer = setTimeout(function () {
           onZoom((mousewheeldelta > 0) ? current.zoom / 1.7 : current.zoom * 1.7, current.x + last_e.clientX * current.zoom, current.y + last_e.clientY * current.zoom, last_e.clientX, last_e.clientY);
           mousewheeldelta = 0;
-          mousewheeltimer = null; }, 70);
+          mousewheeltimer = null;
+        }, 70);
       }
     };
   }
   else {
-    mousewheel = function(e) {
+    mousewheel = function (e) {
       e.preventDefault();
       var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
       onZoom((delta > 0) ? current.zoom / 1.7 : current.zoom * 1.7, current.x + e.clientX * current.zoom, current.y + e.clientY * current.zoom, e.clientX, e.clientY);
@@ -255,27 +260,27 @@ var bigpicture = (function() {
    * KEYBOARD SHORTCUTS
    */
 
-  window.onkeydown = function(e) {
-    if (((e.ctrlKey && !e.altKey || e.metaKey) && (e.keyCode == 61 || e.keyCode == 187 || e.keyCode == 171 || e.keyCode == 107 || e.key == '+' || e.key == '=' ))   // CTRL+PLUS or COMMAND+PLUS 
-    || e.keyCode == 34) {   // PAGE DOWN     // !e.altKey to prevent catching of ALT-GR 
+  window.onkeydown = function (e) {
+    if (((e.ctrlKey && !e.altKey || e.metaKey) && (e.keyCode == 61 || e.keyCode == 187 || e.keyCode == 171 || e.keyCode == 107 || e.key == '+' || e.key == '='))   // CTRL+PLUS or COMMAND+PLUS 
+      || e.keyCode == 34) {   // PAGE DOWN     // !e.altKey to prevent catching of ALT-GR 
       e.preventDefault();
       onZoom(current.zoom / 1.7);
       return;
     }
-    if (((e.ctrlKey && !e.altKey || e.metaKey) && (e.keyCode == 54 || e.keyCode == 189 || e.keyCode == 173 || e.keyCode == 167 || e.keyCode == 109 || e.keyCode == 169 || e.keyCode == 219 || e.key == '-' ))   // CTRL+MINUS or COMMAND+MINUS
-    || e.keyCode == 33) {   // PAGE UP
+    if (((e.ctrlKey && !e.altKey || e.metaKey) && (e.keyCode == 54 || e.keyCode == 189 || e.keyCode == 173 || e.keyCode == 167 || e.keyCode == 109 || e.keyCode == 169 || e.keyCode == 219 || e.key == '-'))   // CTRL+MINUS or COMMAND+MINUS
+      || e.keyCode == 33) {   // PAGE UP
       e.preventDefault();
       onZoom(current.zoom * 1.7);
       return;
     }
     if ((e.ctrlKey && !e.altKey || e.metaKey) && e.keyCode == 70) {         // CTRL+F
       e.preventDefault();
-      setTimeout(function() { query = window.prompt("What are you looking for?", ""); findNext(query); }, 10);
+      setTimeout(function () { query = window.prompt("What are you looking for?", ""); findNext(query); }, 10);
       return;
     }
     if (e.keyCode == 114) {                 // F3
       e.preventDefault();
-      if (results.index == -1) { setTimeout(function() { query = window.prompt("What are you looking for?", ""); findNext(query); }, 10); }
+      if (results.index == -1) { setTimeout(function () { query = window.prompt("What are you looking for?", ""); findNext(query); }, 10); }
       else { findNext(query); }
       return;
     }
@@ -292,14 +297,16 @@ var bigpicture = (function() {
 
   function isContainedByClass(e, cls) { while (e && e.tagName) { if (e.classList.contains(cls)) { return true; } e = e.parentNode; } return false; }
 
-  function getQueryVariable(id) { var params = window.location.search.substring(1).split("&");  for (var i = 0; i < params.length; i++) { var p = params[i].split("="); if (p[0] == id) { return p[1]; } } return(false); }
+  function getQueryVariable(id) { var params = window.location.search.substring(1).split("&"); for (var i = 0; i < params.length; i++) { var p = params[i].split("="); if (p[0] == id) { return p[1]; } } return (false); }
 
   /*
    * API
    */
 
-  return { newText: newText, 
-           current: current, 
-           updateTextPosition: updateTextPosition };
+  return {
+    newText: newText,
+    current: current,
+    updateTextPosition: updateTextPosition
+  };
 
 })();
